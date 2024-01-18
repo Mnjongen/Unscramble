45820205528c210a4b9b142bb90588a50441d7f4
Brent de Jong <Brentdejong2@gmail.com>
2024-01-18 23:15:58 +0100

    Basic implementation of the Anagramizer algorithm

---
+++ Anagramizer/Anagramizer.cs
@@ -0,0 +1,22 @@
+﻿namespace Anagramizer
+{
+    public class Anagramizer
+    {
+        private readonly LetterNode _root = new(false);
+        public void AddWord(ReadOnlySpan<char> word)
+        {
+            _root.AddWord(word);
+        }
+        public void RemoveWord(string word)
+        {
+            _root.RemoveWord(word.AsSpan());
+        }
+        public HashSet<string> GetWords(char[] letters, int maxLength = 32)
+        {
+            var words = new HashSet<string>(100);
+
+            _root.FindWords(words, letters.ToList(), maxLength, new Stack<char>(maxLength));
+            return words;
+        }
+    }
+}
